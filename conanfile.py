76cc2cecc84a089c1b9c9aa41f66510cc004aebc
daphned1 <daphned1@users.noreply.github.com>
2024-02-01 20:19:40 +0000

    [create-pull-request] automated change

--- conanfile.py
+++ conanfile.py
@@ -22,14 +22,14 @@ import os
 required_conan_version = ">=2.0.6"
 
 
-class libhal___device___conan(ConanFile):
-    name = "libhal-__device__"
+class libhal__conan(ConanFile):
+    name = "libhal-"
     version = "0.0.1"
     license = "Apache-2.0"
     url = "https://github.com/conan-io/conan-center-index"
-    homepage = "https://github.com/libhal/libhal-__device__"
-    description = ("A collection of drivers for the __device__")
-    topics = ("__device__", "libhal", "driver")
+    homepage = "https://github.com/libhal/libhal-"
+    description = ("A collection of drivers for the ")
+    topics = ("", "libhal", "driver")
     settings = "compiler", "build_type", "os", "arch"
     exports_sources = ("include/*", "tests/*", "LICENSE", "CMakeLists.txt",
                        "src/*")
@@ -91,5 +91,5 @@ class libhal___device___conan(ConanFile):
         cmake.install()
 
     def package_info(self):
-        self.cpp_info.libs = ["libhal-__device__"]
-        self.cpp_info.set_property("cmake_target_name", "libhal::__device__")
+        self.cpp_info.libs = ["libhal-"]
+        self.cpp_info.set_property("cmake_target_name", "libhal::")
